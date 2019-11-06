82d4826d7dcb8db2dd96e049ab267f8581e2fe0c
Yohay <yohay@superads.cn>
2019-11-06 20:02:43 +0200

    heroku

--- app.js
+++ app.js
@@ -23,7 +23,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(require('method-override')());
-// app.use(express.static(__dirname + '/public'));
+//set the template engine ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
 
 // app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
 
@@ -87,4 +95,31 @@ app.use(function (err, req, res, next) {
 // finally, let's start our server...
 const server = app.listen(process.env.PORT || 3000, function () {
     console.log('Listening on port ' + server.address().port);
+});
+
+const io = require("socket.io")(server);
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    //default username
+    socket.username = "Anonymous";
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    });
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    });
+
+    //listen on typing
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', {username : socket.username})
+    })
 });
