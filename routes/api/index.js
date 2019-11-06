4719f8eaf56c383a6272582e805881ac3dd7237a
Yohay <yohay@superads.cn>
2019-11-06 14:01:22 +0200

    initial commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,8 @@
 var router = require('express').Router();
 
-router.use('/', require('./users'));
-router.use('/profiles', require('./profiles'));
-router.use('/articles', require('./articles'));
-router.use('/tags', require('./tags'));
+// router.use('/', require('./users'));
+router.use('/jobs', require('./jobs'));
+
 
 router.use(function(err, req, res, next){
   if(err.name === 'ValidationError'){
